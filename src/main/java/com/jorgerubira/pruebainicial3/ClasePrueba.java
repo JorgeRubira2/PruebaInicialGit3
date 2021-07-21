9eb98484d5230e24e241b3a0eee7e138358784bd
JorgeRubira2 <87339554+JorgeRubira2@users.noreply.github.com>
2021-07-21 09:35:39 +0200

    Update ClasePrueba.java

--- src/main/java/com/jorgerubira/pruebainicial3/ClasePrueba.java
+++ src/main/java/com/jorgerubira/pruebainicial3/ClasePrueba.java
@@ -1,16 +1,18 @@
 package com.jorgerubira.pruebainicial3;
 
 public class ClasePrueba {
+    //Comentario 3
     public int suma(int a, int b){
         return a+b;
     }
     public int resta(int a, int b){
         return a-b;
     }
+    
     public int multiplicacion(int a, int b){
         return a*b;
     }
-    
+    //Comentario 4
     public int division(int a, int b){
         return a/b;
     }    
